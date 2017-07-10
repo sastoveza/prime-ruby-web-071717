216342eabca63d7062d50f446dd8ea5d33da6233
Stacey Astoveza <staceyastoveza@transfix.io>
2017-07-10 21:21:14 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(num)
+  return false if num != num.abs || num == 0 || num == 1
+
+  primes = (2..num).to_a
+
+  for divisor in primes
+    break if divisor * divisor > num
+    return false if num % divisor == 0
+  end
+
+  true
+end
+
+puts prime?(-5)
